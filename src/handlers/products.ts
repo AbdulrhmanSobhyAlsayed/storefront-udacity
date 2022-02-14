0418cff9a78a54e573d6772a63c81eb3adb924a9
AbdulrhmanSobhyAlsayed <asobalsayed@gmail.com>
2022-02-14 23:49:51 +0300

    add products and users handlers

---
+++ src/handlers/products.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from "express";
+import { ProductModel } from "../models/product";
+
+const productRoutes = (app: express.Application) => {
+  app.get("/products", listProducts);
+  app.get("/products/:id", showProduct);
+  app.post("/products", createProduct);
+};
+
+const product = new ProductModel();
+
+const listProducts = async (_req: Request, res: Response) => {
+  try {
+    const products = await product.index();
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const showProduct = async (req: Request, res: Response) => {
+  try {
+    const displayedProduct = await product.show(req.params.id);
+
+    res.status(200).json(displayedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const createdProduct = await product.create(req.body);
+
+    res.status(200).json(createdProduct);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
+export default productRoutes;
---
+++ src/handlers/users.ts
@@ -0,0 +1,54 @@
+import express, { Request, Response } from "express";
+import { UserModel } from "../models/user";
+import bcrypt from "bcrypt";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { BCRYPT_PASSWORD } = process.env;
+
+const userRoutes = (app: express.Application) => {
+  app.get("/users", listUsers);
+  app.get("/users/:id", showUser);
+  app.post("/users", createUser);
+};
+
+const user = new UserModel();
+
+const listUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await user.index();
+
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const showUser = async (req: Request, res: Response) => {
+  try {
+    const displayedUser = await user.show(req.params.id);
+
+    res.status(200).json(displayedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    req.body.password = bcrypt.hashSync(
+      req.body.password + BCRYPT_PASSWORD,
+      10
+    );
+    const createdUser = await user.create(req.body);
+
+    res.status(200).json(createdUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
+export default userRoutes;
